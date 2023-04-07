a6e811972f8a4eb83f8d8a27485e185e3f913a6b
VoThiHoaiThanh <127926036+VoThiHoaiThanh@users.noreply.github.com>
2023-04-07 15:13:39 +0700

    test

--- TCP Client/TCP Client/Form1.Designer.cs
+++ TCP Client/TCP Client/Form1.Designer.cs
@@ -28,20 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textInput = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(308, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textInput
+            // 
+            this.textInput.Location = new System.Drawing.Point(603, 73);
+            this.textInput.Name = "textInput";
+            this.textInput.Size = new System.Drawing.Size(100, 26);
+            this.textInput.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(353, 335);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 2;
             // 
             // TCP_Client
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textInput);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "TCP_Client";
             this.Text = "TCP Client";
+            this.Load += new System.EventHandler(this.TCP_Client_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textInput;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- TCP Client/TCP Client/Form1.cs
+++ TCP Client/TCP Client/Form1.cs
@@ -4,7 +4,10 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +18,50 @@ namespace TCP_Client
         public TCP_Client()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            int port = 11000;
+            TcpClient client = new TcpClient();
+            client.Connect(ip, port);
+            MessageBox.Show("client connected!!");
+            NetworkStream ns = client.GetStream();
+            Thread thread = new Thread(o => ReceiveData((TcpClient)o));
+
+            thread.Start(client);
+
+            string s;
+            while (!string.IsNullOrEmpty((s = textInput.Text)))
+            {
+                byte[] buffer = Encoding.UTF32.GetBytes(s);
+                ns.Write(buffer, 0, buffer.Length);
+            }
+
+            client.Client.Shutdown(SocketShutdown.Send);
+            thread.Join();
+            ns.Close();
+            client.Close();
+          //  Console.WriteLine("disconnect from server!!");
+          //  Console.ReadKey();
+        }
+
+        private void TCP_Client_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        static void ReceiveData(TcpClient client)
+        {
+            NetworkStream ns = client.GetStream();
+            byte[] receivedBytes = new byte[1024];
+            int byte_count;
+
+            while ((byte_count = ns.Read(receivedBytes, 0, receivedBytes.Length)) > 0)
+            {
+                Console.Write(Encoding.UTF32.GetString(receivedBytes, 0, byte_count));
+            }
         }
     }
 }
