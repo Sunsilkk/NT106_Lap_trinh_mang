5d7cc5f701a0a8a1adcc902a607ebd6b327a0b83
Sunsilkk <94736191+Sunsilkk@users.noreply.github.com>
2023-04-22 21:43:31 +0700

    Add files via upload

---
+++ Week6/API test/Week6.2/BTT6/BTT6/API Test.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Drawing;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTT6
+{
+    public partial class Form1 : Form
+    {
+        private HttpClient client = new HttpClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void bt_Get_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync("https://jsonplaceholder.typicode.com/todos").ConfigureAwait(false);
+                string responseString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+                // Use BeginInvoke to update the UI on the main thread
+                BeginInvoke((Action)(() =>
+                {
+                    ToDoList.Text = responseString;
+                }));
+            }
+            catch (HttpRequestException ex)
+            {
+                // Use BeginInvoke to update the UI on the main thread
+                BeginInvoke((Action)(() =>
+                {
+                    ToDoList.Text = ex.Message;
+                }));
+            }
+        }
+
+        private void bt_Clear_Click(object sender, EventArgs e)
+        {
+            ToDoList.Text = null;
+        }
+
+        private void bt_exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void bt_exit_MouseMove(object sender, MouseEventArgs e)
+        {
+            bt_exit.BackColor = Color.DarkRed;
+        }
+
+        private void bt_exit_MouseLeave(object sender, EventArgs e)
+        {
+            bt_exit.BackColor = Color.IndianRed;
+        }
+    }
+}
