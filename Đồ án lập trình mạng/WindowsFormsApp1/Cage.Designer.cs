6ad25dcfb14230f94e4cf1ef34e9320f8ed27763
VoThiHoaiThanh <127926036+VoThiHoaiThanh@users.noreply.github.com>
2023-06-05 19:17:51 +0700

    update cages

--- Đồ án lập trình mạng/WindowsFormsApp1/Cage.Designer.cs
+++ Đồ án lập trình mạng/WindowsFormsApp1/Cage.Designer.cs
@@ -28,12 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Cage";
+            dgv_Cages = new System.Windows.Forms.DataGridView();
+            ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            PET_ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            PET_TYPE_ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            EMPTY = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dgv_Cages).BeginInit();
+            SuspendLayout();
+            // 
+            // dgv_Cages
+            // 
+            dgv_Cages.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgv_Cages.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] { ID, PET_ID, PET_TYPE_ID, EMPTY });
+            dgv_Cages.Location = new System.Drawing.Point(15, 39);
+            dgv_Cages.Name = "dgv_Cages";
+            dgv_Cages.RowHeadersWidth = 62;
+            dgv_Cages.RowTemplate.Height = 33;
+            dgv_Cages.Size = new System.Drawing.Size(760, 408);
+            dgv_Cages.TabIndex = 0;
+            // 
+            // ID
+            // 
+            ID.HeaderText = "ID";
+            ID.MinimumWidth = 8;
+            ID.Name = "ID";
+            ID.Width = 150;
+            // 
+            // PET_ID
+            // 
+            PET_ID.HeaderText = "PET ID";
+            PET_ID.MinimumWidth = 8;
+            PET_ID.Name = "PET_ID";
+            PET_ID.Width = 150;
+            // 
+            // PET_TYPE_ID
+            // 
+            PET_TYPE_ID.HeaderText = "PET TYPE ID";
+            PET_TYPE_ID.MinimumWidth = 8;
+            PET_TYPE_ID.Name = "PET_TYPE_ID";
+            PET_TYPE_ID.Width = 150;
+            // 
+            // EMPTY
+            // 
+            EMPTY.HeaderText = "EMPTY";
+            EMPTY.MinimumWidth = 8;
+            EMPTY.Name = "EMPTY";
+            EMPTY.Width = 150;
+            // 
+            // Cage
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            Controls.Add(dgv_Cages);
+            Name = "Cage";
+            Size = new System.Drawing.Size(800, 450);
+            ((System.ComponentModel.ISupportInitialize)dgv_Cages).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgv_Cages;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PET_ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PET_TYPE_ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn EMPTY;
     }
 }
--- Đồ án lập trình mạng/WindowsFormsApp1/Cage.cs
+++ Đồ án lập trình mạng/WindowsFormsApp1/Cage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using WindowsFormsApp1.Class;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApp1;
+using WindowsFormsApp1.Class;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Pet_Management
 {
     public partial class Cage : UserControl
     {
+        Supabase.Client supabase;
         public Cage()
         {
             InitializeComponent();
+            InitializeSupabase();
+        }
+        private void InitializeSupabase()
+        {
+            var url = "https://hpvdlorgdoeaooibnffe.supabase.co";
+            var key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhwdmRsb3JnZG9lYW9vaWJuZmZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ0MzA3ODMsImV4cCI6MjAwMDAwNjc4M30.toI_Vn6TKJFbM8YBT3qbYzLCiAfQtj9VHKw53qQNYOU";
+
+            var options = new Supabase.SupabaseOptions
+            {
+                AutoConnectRealtime = true
+            };
+
+            supabase = new Supabase.Client(url, key, options);
+
+        }
+        private async Task<List<Cages>> Getcage()
+        {
+            var result = await supabase.From<Cages>().Get();
+            var cages = result.Models;
+            return cages;
+        }
+        private async Task LoadData()
+        {
+
+            var cages = await Getcage();
+            dgv_Cages.Rows.Add(cages);
+            try
+            {
+                foreach (var cage in cages)
+                {
+                    MessageBox.Show("hffjv", "hffkjvkd");
+                    dgv_Cages.Rows.Add(cage.Id, cage.Pet_id, cage.Pet_type_id, cage.empty);
+                }
+            }
+            catch (Exception ex) { }
+        }
+        private void Cage_Load(object sender, EventArgs e)
+        {
+            LoadData();
+
         }
     }
 }
