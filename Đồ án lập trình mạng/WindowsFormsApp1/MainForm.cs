1551e50d57187af23e90454acf20222e73ecf63b
Sunsilkk <94736191+Sunsilkk@users.noreply.github.com>
2023-06-07 01:05:39 +0700

    embed font to winform

--- Đồ án lập trình mạng/WindowsFormsApp1/MainForm.cs
+++ Đồ án lập trình mạng/WindowsFormsApp1/MainForm.cs
@@ -1,5 +1,9 @@
 ﻿using Pet_Management;
 using System;
+using System.Drawing.Text;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp1
@@ -30,7 +34,30 @@ namespace WindowsFormsApp1
 
         private void MainForm_Load(object sender, EventArgs e)
         {
+            // Tạo đối tượng PrivateFontCollection
+            PrivateFontCollection privateFonts = new PrivateFontCollection();
 
+            // Truy xuất tài nguyên nhúng của font
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Stream fontStream = assembly.GetManifestResourceStream("WindowsFormsApp1.Resources.Qaz-ZV74m.ttf");
+
+            // Đọc dữ liệu font từ luồng
+            byte[] fontData = new byte[fontStream.Length];
+            fontStream.Read(fontData, 0, (int)fontStream.Length);
+            fontStream.Close();
+
+            // Thêm font vào PrivateFontCollection
+            IntPtr fontPtr = System.Runtime.InteropServices.Marshal.AllocCoTaskMem(fontData.Length);
+            System.Runtime.InteropServices.Marshal.Copy(fontData, 0, fontPtr, fontData.Length);
+            privateFonts.AddMemoryFont(fontPtr, fontData.Length);
+
+            // Sử dụng font trong các điều khiển WinForms
+           // Font customFont = new Font(privateFonts.Families[0], 12f, FontStyle.Regular);
+           // yourLabel.Font = customFont;
+           // yourButton.Font = customFont;
+
+            // Giải phóng bộ nhớ cho font
+            System.Runtime.InteropServices.Marshal.FreeCoTaskMem(fontPtr);
         }
 
         private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
